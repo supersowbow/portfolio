5f5316d86c4bc0925de524926f89a7a99aac7f4e
supersowbow <kybow@protonmail.com>
2017-11-09 20:24:41 -0600

    template configuration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from secret import secrets
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include('home.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^contact/', include('contact.urls')),
 ]
